f57f6d4f9b38a7284d7fbadf30f62f18a46935a5
Nguyen Hai Ha <td12a11haiha@gmail.com>
2022-08-15 18:32:03 +0700

    update path backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const route = require('./routes/routes')
 const dotenv = require('dotenv')
+const path = require('path')
 
 dotenv.config()
 
